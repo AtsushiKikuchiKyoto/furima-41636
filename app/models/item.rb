03935fca4575b0ac505da31f215b0838eeacecd7
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-17 22:35:51 +0900

    画像投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :lead_time_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
+  has_one_attached :image
 
   belongs_to :user
 
