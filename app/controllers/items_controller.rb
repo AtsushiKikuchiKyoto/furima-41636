c2c45c176d929216a33d5e935d3d02a30cc3b646
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-17 21:06:22 +0900

    テーブルへの保存(name,categoryのみ)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,17 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category_id).merge(user_id: current_user.id)
+  end
+
 end
