17d150744f3bb3d91a2e4966f3bbde153b93f194
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-17 17:44:49 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_16_093317) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_17_084203) do
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "content"
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "postage_id"
+    t.integer "prefecture_id"
+    t.integer "lead_time_id"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -34,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_16_093317) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
