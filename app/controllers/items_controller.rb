92c0ec489f19ddee5d3b628bc198bb5b6c23c915
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-17 22:22:26 +0900

    Item保存機能(image以外)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,17 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to root_path
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :category_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :content, :category_id, :condition_id, :postage_id, :prefecture_id, :lead_time_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :lead_time
+
+  validates :name, presence: true
+  validates :content, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :lead_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 
   belongs_to :user
 
