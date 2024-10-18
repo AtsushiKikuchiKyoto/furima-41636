859a4a27737c829b548b058f12bdce1ccccf87dd
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-18 18:00:45 +0900

    モデルテスト漏れ追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -75,5 +75,10 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include "Price must be an integer"
     end
+    it 'ユーザーが紐づいてなければ登録できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
   end
 end
