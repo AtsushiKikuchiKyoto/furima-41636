53c2ac35f44c2a968ebca242750b5d60ea15ee38
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-19 20:59:02 +0900

    編集失敗時の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,9 +34,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
