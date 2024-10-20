0597900de2d20c93858170383865bfe2e8695927
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-20 19:48:33 +0900

    フォームオブジェクト
    (ビューはエラーなし)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new
   end
 
   def create
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:post_code, :prefecture_id, :city, :street_address, :building, :tel).merge(user_id: current_user.id, item_id:params[:item_id])
   end
 
 end
---
+++ app/models/order_delivery.rb
@@ -0,0 +1,17 @@
+class OrderDelivery
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :street_address, :building, :tel
+
+  with_options presence: true do
+    validates :city, :street_address
+    validates :user_id, :item_id
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :tel, format: { with: /\A\d{10,11}\z/, message: "is invalid. Enter 10 or 11 numbers without hyphen(-)" }
+  end
+
+  def save
+    order = Oeder.create(user_id: user_id, item_id: item_id)
+    Delivery.create(post_code: post_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building: building, tel: tel, order_id: order.id)
+  end
+end
